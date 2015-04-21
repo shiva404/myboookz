3812e5b0d457416e6344414c399d67e6e13fdb7b
Shivakumar <shiva.n404@gmail.com>
2015-04-21 09:05:05 +0530

    -Instead of IP address putting domain name

--- config/config.prd.js
+++ config/config.prd.js
@@ -6,12 +6,12 @@ config.hostname = 'dev.example.com';
 config.facebook = {}
 config.facebook.clientID = '136567019835635'
 config.facebook.clientSecret = '925042e37ac32f1591cc9e18cda99297'
-config.facebook.callbackURL = 'http://54.251.185.219:3000/auth/fb/callback'
+config.facebook.callbackURL = 'http://bookforborrow.com/auth/fb/callback'
 
 config.goodreads = {}
 config.goodreads.clientID = 'QLM3lL2nqXe4LujHQt12A'
 config.goodreads.clientSecret = 'Aegcm52QdTinBh6g5fZe81S5cVYdKk9P6IDVS38pDOw'
-config.goodreads.callbackURL = 'http://54.251.185.219:3000/auth/goodreads/callback'
+config.goodreads.callbackURL = 'http://bookforborrow.com/auth/goodreads/callback'
 
 config.neo4jservice = {}
 config.neo4jservice.baseurl = 'http://54.251.185.219:8080/neo4j/v1'
